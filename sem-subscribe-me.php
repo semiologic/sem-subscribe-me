edeb79ca5f7eb1693d510d8fc6cab6b2e029ca17
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-04-02 14:31:37 +0000

    implement widget cache flush on sidebars_widgets update, and force such a call on every POST action on that page
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-subscribe-me/trunk@985 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-subscribe-me.php
+++ sem-subscribe-me.php
@@ -52,6 +52,9 @@ class subscribe_me
 				'update_option_show_on_front',
 				'update_option_page_on_front',
 				'update_option_page_for_posts',
+				'update_option_sidebars_widgets',
+				'update_option_sem5_options',
+				'update_option_sem6_options',
 				) as $hook)
 		{
 			add_action($hook, array('subscribe_me', 'clear_cache'));
